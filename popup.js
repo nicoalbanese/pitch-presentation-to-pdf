fb470e434ef9fe484f39a5b0ea5dbe005a72fd9e
Nico Albanese <gcalbanese96@gmail.com>
2023-03-23 21:45:38 +0000

    functionality done...

--- popup.js
+++ popup.js
@@ -4,6 +4,12 @@ document.getElementById("exportBtn").addEventListener("click", () => {
   
       // Send a message to the content script to start the export process
       chrome.tabs.sendMessage(tab.id, { action: "exportPresentation" });
+
+      const statusMessage = document.getElementById("statusMessage");
+      const instructions = document.getElementById("instructions");
+      instructions.style.display = "none";
+      statusMessage.innerText = "EXPORTING";
+      statusMessage.style.display = "block"
     });
   });
   
